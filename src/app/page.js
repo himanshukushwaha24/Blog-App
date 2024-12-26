8c369cbeaea6b26c1f80fb0b3e575a2c0c11d6a4
himanshukushwaha24 <himanshu.kushwaha@xtendedspace.com>
2024-12-26 19:22:58 +0530

    Initialize Project

---
+++ src/app/page.js
@@ -0,0 +1,15 @@
+// page.js
+
+import React from "react";
+import BlogList from "@/Components/BlogList";
+import Createblog from "@/pages/CreateBlog";
+
+const page = () => {
+  return (
+    <>
+      <BlogList />
+    </>
+  );
+};
+
+export default page;
